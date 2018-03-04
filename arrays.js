cc4f82a2d6452c74fc1e4e31f81aa2afd132083b
Mohammed Noori <kmanoorib@gmail.com>
2018-03-04 20:11:07 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningofArray(){
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
